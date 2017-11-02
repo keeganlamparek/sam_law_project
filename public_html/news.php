93b058c496a99bd6a8f3eb42609df2d11ea59265
DESKTOP-KF80L94\Keegan <keeganlamparek@gmail.com>
2017-11-01 23:06:28 -0500

    testing webpage

---
+++ public_html/news.php
@@ -0,0 +1,54 @@
+<!--Created by Josh Dietrich-->
+
+<!DOCTYPE html>
+<html lang='en'>
+
+<head>
+	<meta charset="utf-8">
+	<title>Game Space: News</title>
+	<link rel='icon' href='img/icon.png'/>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+   	 <!-- Main CSS styles for Bootstrap -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+
+   	 <!-- Optional theme add-on -->
+	<link rel="stylesheet" href="https://bootswatch.com/superhero/bootstrap.min.css">
+
+	<link rel="stylesheet" href="css/custom.css">
+</head>
+
+<body>
+ <!-- Navigation Bar -->
+ <?php include "../resources/navigation.php"; ?>
+
+      <div class="jumbotron">
+        <div class="container">
+          <h1 class="display-3">Game Space News</h1>
+        </div>
+      </div>
+
+	<section>
+	<div class="container">
+	<div class=" col-xs-12">
+		<p>As our clientele continues to grow, as do our stores. We have recently begun opening stores in Texas, as this was a big market for our online services. There are stores in Houston, San Antonio, Dallas, Austin, and Arlington as of right now, and there are plans to continue making more. If you don't have a store near you and you would like one, let us know via email.</p>
+
+		<p>We have also expanded our game selection to include games for the Nintendo Switch. There has been much demand for this, and we are here to serve the customer. These games can now be bought in many of our stores, as well as on our online order forms.</p>
+
+		<p>We became a company on August 31, 2017 in the midst of a gaming storm. Our main purpose is to fulfill the needs of gamers nationwide, and eventually worldwide</p>
+	</div>
+	</div>
+	</section>
+
+	<footer>
+		<div style='text-align: center; padding-top: 50px'>
+			&copy; Josh Dietrich and Sam Law 2017
+		</div>
+	</footer>
+
+
+    <!-- Scripts required to make Bootstrap work better. -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script type="text/javascript" src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  </body>
+</html>
